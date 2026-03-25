9e9d821b7494766210916f9526dbb33177a242e5
se1collapsar <1902018278@qq.com>
2026-03-25 13:33:06 +0800

    add config and backup modle 25-03-2026 13:33

--- config.py
+++ config.py
@@ -2,14 +2,20 @@ class Config:
     """
     全局配置文件（所有超参数统一管理）
     """
+    # ---------- 训练 ----------
+    EPISODES = 50000
+
+    # ---------- 模型 ----------
+    MODEL_DIR = "models"
+    MAX_BACKUP_MODELS = 10
 
     # ---------- 环境 ----------
     GRID_SIZE = 10
-    MAX_STEPS_WITHOUT_FOOD = 300
+    MAX_STEPS_WITHOUT_FOOD = 200
 
     # ---------- 奖励 ----------
-    REWARD_DEATH = -100
-    REWARD_EAT = 1
+    REWARD_DEATH = -20
+    REWARD_EAT = 10
     REWARD_WIN = 50
     REWARD_STEP = -0.01
     REWARD_DISTANCE_FACTOR = 0.1
